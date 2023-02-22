513c9b54e2a746853dd37860a0fc922d0c19c696
skashyap41050 <75583494+skashyap41050@users.noreply.github.com>
2023-02-22 16:15:23 +0530

    Update hub_conf.py

--- hub_conf.py
+++ hub_conf.py
@@ -19,7 +19,8 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor(),
 )
 batch_size = 64
-
+def add(a,b):
+    return a+b
 # Create data loaders.
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
