b448c86a883d66673b86a0eb3c8ad4dae1f997dd
skashyap41050 <75583494+skashyap41050@users.noreply.github.com>
2023-02-22 17:11:34 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -28,11 +28,9 @@ def test_data():
     test_dataloader = DataLoader(test_data, batch_size=batch_size)
     return train_dataloader,test_dataloader
 
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
-    
+train_dataloader,test_dataloader = test_data()
+
+
     
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Using {device} device")
