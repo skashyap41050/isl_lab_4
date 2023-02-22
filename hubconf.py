2f5e595e6b743f940431bad679c6812b54ba0105
skashyap41050 <75583494+skashyap41050@users.noreply.github.com>
2023-02-22 16:41:53 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -68,7 +68,6 @@ def train(dataloader, model, loss_fn, optimizer):
 
         if batch % 100 == 0:
             loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
             
 def test(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
@@ -83,7 +82,7 @@ def test(dataloader, model, loss_fn):
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
     
     
     
@@ -92,7 +91,6 @@ for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     test(test_dataloader, model, loss_fn)
-print("Done!")
 
 
 torch.save(model.state_dict(), "model.pth")
@@ -121,4 +119,4 @@ x, y = test_data[0][0], test_data[0][1]
 with torch.no_grad():
     pred = model(x)
     predicted, actual = classes[pred[0].argmax(0)], classes[y]
-    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+    
