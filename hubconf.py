247140bb4ec0dd8e55429c5ef556bfe1bb287f97
skashyap41050 <75583494+skashyap41050@users.noreply.github.com>
2023-02-22 17:09:17 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -23,8 +23,10 @@ test_data = datasets.FashionMNIST(
 batch_size = 64
 
 # Create data loaders.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+def test_data():
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    return train_dataloader,test_dataloader
 
 for X, y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
